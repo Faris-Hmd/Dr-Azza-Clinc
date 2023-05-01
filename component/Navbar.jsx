7aeb6630c9f6eb170a8141b094d3d36536d329ec
Faris-Hamad <bluocat4@gmail.com>
2023-05-01 08:00:09 +0200

    improve side menu

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Offcanvas } from "react-bootstrap";
+import { Accordion, Button, Col, Container, Offcanvas } from "react-bootstrap";
 import styles from "../styles/Navbar.module.css";
 import { useState } from "react";
 import { FaBars, FaHome } from "react-icons/fa";
@@ -15,7 +15,7 @@ import {
 import Link from "next/link";
 import { BiBookmark } from "react-icons/bi";
 
-const link = [
+const links = [
   {
     name: "HOME",
     href: "/",
@@ -62,13 +62,15 @@ const Navbar = () => {
           <Offcanvas.Title>Menu</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body className="flex gap-3 justify-content-start">
-          <Container className="p-0 rounded overflow-hidden">
-            {link.map((link, index) => {
+          <Container className="p-0 rounded border">
+            {links.map((link, index) => {
               return (
                 <Col xs={12} onClick={handleClose} key={index}>
                   <Link
                     href={link.href}
-                    className="w-100 Link hover flex-r p-2 rounded border mt-1"
+                    className={`w-100 Link hover flex-r borde p-3 ${
+                      index + 1 !== links.length && "border-b"
+                    }`}
                   >
                     {link.icon}
                     {link.name}
@@ -78,38 +80,53 @@ const Navbar = () => {
             })}
           </Container>
           <Container className="p-0 border rounded">
-            <Col xs={12} onClick={handleClose}>
-              <Link
-                href="Products/Add"
-                className="w-100 Link hover flex-r p-3 border-b"
-              >
-                <BsPlus size={"20px"} className="me-3" />
-                Add Product
-              </Link>
-              <Link
-                href="/Products/Edit"
-                className="w-100 Link hover flex-r p-3 border-b"
-              >
-                <BsPencil size={"20px"} className="me-3" />
-                Edit Product
-              </Link>
-            </Col>
-            <Col xs={12} onClick={handleClose}>
-              <Link
-                href="/Articles/Add"
-                className="w-100 Link hover flex-r p-3 border-b"
-              >
-                <BsPlus size={"20px"} className="me-3" />
-                Add Article
-              </Link>
-              <Link
-                href="/Articles/Edit"
-                className="w-100 Link hover flex-r p-3"
-              >
-                <BsPencil size={"20px"} className="me-3" />
-                Edit Article
-              </Link>
-            </Col>
+            <Accordion flush>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                  <BsPlus size={"20px"} className="me-3" />
+                  ADD
+                </Accordion.Header>
+                <Accordion.Body className="bg- p-0">
+                  <Col xs={12} onClick={handleClose}>
+                    <Link
+                      href="/Articles/Add"
+                      className="w-100 Link hover flex-r p-2 border-b"
+                    >
+                      Add Article
+                    </Link>
+                    <Link
+                      href="Products/Add"
+                      className="w-100 Link hover flex-r p-2 border-b"
+                    >
+                      Add Product
+                    </Link>
+                  </Col>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>
+                  <BsPencil size={"20px"} className="me-3" />
+                  EDIT
+                </Accordion.Header>
+
+                <Accordion.Body className="p-0">
+                  <Col xs={12} onClick={handleClose}>
+                    <Link
+                      href="/Products/Edit"
+                      className="w-100 Link hover flex-r p-2 border-b"
+                    >
+                      Edit Product
+                    </Link>
+                    <Link
+                      href="/Articles/Edit"
+                      className="w-100 Link hover flex-r p-2"
+                    >
+                      Edit Article
+                    </Link>
+                  </Col>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </Container>
         </Offcanvas.Body>
       </Offcanvas>
